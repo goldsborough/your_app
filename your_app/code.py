ee2038fcc4b2b656224f37b1027111ee789667d4
petergoldsborough <petergoldsborough@hotmail.com>
2015-08-11 01:40:57 +0200

    Finished implementation and documentation of source code

---
+++ your_app/code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Your application's badass source code.
+"""
+
+def square(x):
+	"""
+	Squares a value.
+
+	Equivalent to calling x**2.
+
+	Arguments:
+		x (int|float): The value to square.
+
+	Returns:
+		The squared value.
+	"""
+
+	return x * x
